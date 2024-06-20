9941e81908a7e74023824a192d6f22ae6c9365f3
Pedro Henrique Pinheiro de Oliveira <pedrocaseis@hotmail.com>
2024-06-20 12:10:02 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,8 @@
+var letra = prompt(`Digite uma letra ai meu patrão`)
+
+if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u") {
+    console.log(`A letra é vogal`)
+}
+else {
+    console.log(`A letra é consoante`)
+}
